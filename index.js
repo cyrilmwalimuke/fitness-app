3c81d9f7708c6fc2e3c0c340cc81316d21fad53a
cyrilmwalimuke <cyrilmwalimuke@gmail.com>
2025-08-28 15:54:43 +0300

    random commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
-import Exercise from "./workout.js";
+import Exercise from "./exercise.js";
+
 import Workout from "./workout.js";
 
 
