5e7388e0381569c49a329554e8ca8822a5d941d9
cyrilmwalimuke <cyrilmwalimuke@gmail.com>
2025-10-06 22:14:41 +0300

    random commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose"
 import Exercise from "./exercise.js";
 
 import Workout from "./workout.js";
-import CustomWorkout from "./customworkout.js";
+import CustomWorkout from "./customWorkout.js";
 
 
 const app = express()
